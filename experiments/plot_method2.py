7cbfe6b1c35272f7effd6d83eb1fdb2fee1979b3
Mengjie Zhou <mengjiezhou2018@outlook.com>
2024-01-30 16:36:07 +0800

    arrangement

--- experiments/plot_method2.py
+++ experiments/plot_method2.py
@@ -11,18 +11,18 @@ plt.rcParams.update({"font.size":22})
 
 if __name__ == "__main__":
     location_name = 'hudsonriver5kU' # change area here
-    model_name1 = 'resnet_asam' # change model here
-    model_name2 = 'resnetsafa_polar_asam_simple'
+    model_name1 = 'resnetsafa_dgcnn_asam_2to3_up_16' # change model here
+    model_name2 = 'resnetsafa_dgcnn_asam_2to3_up'
 
-    recall_es = np.load(os.path.join('results', location_name+'_'+model_name1+'_'+'none'+'.npy'))
+    recall_base = np.load(os.path.join('results', location_name+'_'+model_name1+'_'+'none'+'.npy'))
     recall_ours = np.load(os.path.join('results', location_name+'_'+model_name2+'_'+'none'+'.npy'))
 
-    x = np.arange(1, len(recall_es)+1) / 50
+    x = np.arange(1, len(recall_base)+1) / 50
     markevery = [0, len(x)//4*1, len(x)//4*2, len(x)//4*3, len(x)-1]
     # display
     plt.figure()
-    plt.plot(x,recall_es,color='red',linewidth=2,marker='*',markersize=5,markevery=markevery,
-            linestyle='dashed',label='ES - {:.2f}%'.format(recall_es[0]))
+    plt.plot(x,recall_base,color='red',linewidth=2,marker='*',markersize=5,markevery=markevery,
+            linestyle='dashed',label='Baseline - {:.2f}%'.format(recall_base[0]))
     plt.plot(x,recall_ours,color='blue',linewidth=2,marker='o',markersize=5,markevery=markevery,
             linestyle='solid',label='Ours - {:.2f}%'.format(recall_ours[0]))
     plt.xlabel('k (% of the dataset)')
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     plt.ylim(40,101)
     plt.legend(loc=4)
     plt.grid(linestyle='dashed', linewidth=0.5)
-    plt.savefig(os.path.join('results','chapter04',location_name+'_method.pdf'),bbox_inches='tight')
+    plt.savefig(os.path.join('results','chapter06','multi',location_name+'_method.pdf'),bbox_inches='tight')
     plt.show()
 
 
